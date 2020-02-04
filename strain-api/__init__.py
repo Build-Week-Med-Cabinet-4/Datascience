8c87e551aa719b412f23d9d9d743f2ba9ec6de22
tyler9937 <tyler9937@users.noreply.github.com>
2020-02-04 16:23:57 -0500

    initial API for predicting recommendations

---
+++ strain-api/__init__.py
@@ -0,0 +1,8 @@
+# Importing from api.py file
+from .api import create_api
+
+# Saving create api func as global API var
+API = create_api()
+
+#set FLASK_APP=strain-api:API
+#import pdb; pdb.set_trace()
