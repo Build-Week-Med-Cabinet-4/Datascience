56a6fd49ee0f1a24108a2d33f83822bc3fe9c5d3
tyler9937 <tyler9937@users.noreply.github.com>
2020-02-07 00:30:59 -0500

    test

--- strain-api/models.py
+++ strain-api/models.py
@@ -61,8 +61,8 @@ class Predictor():
     def __init__(self):
         #try:
         # print('Loading models from expected local directory')
-        self.nn = joblib.load(open('Models/nn.pkl', 'rb'))
-        self.tfidf = joblib.load(open('Models/tfidf.pkl', 'rb'))
+        self.nn = joblib.load(open('./strain-api/Models/nn.pkl', 'rb'))
+        self.tfidf = joblib.load(open('./strain-api/Models/tfidf.pkl', 'rb'))
         # print('Loaded Successfully')
 
     def predict(self, input_text, output_size):
