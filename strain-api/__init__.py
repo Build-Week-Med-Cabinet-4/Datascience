83b7ef5053943ce301948b8ffdd7b9b5d017e1ff
Tyler Russin <tylerrussin2@gmail.com>
2022-02-06 23:58:43 -0800

    authorization attempt

--- strain-api/__init__.py
+++ strain-api/__init__.py
@@ -1,9 +1,9 @@
-# Importing from api.py file
-from .api import create_api
+from .api import strain_api
 
 # Saving create api func as global API var
-API = create_api()
+API = strain_api()
 
 # Helpful code for development
+# set FLASK_ENV=development
 # set FLASK_APP=strain-api:API
 # import pdb; pdb.set_trace()
